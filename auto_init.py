b79e6d516062267d60ac66f0600a0c0866765f0f
kenwa <donggenghua@gmail.com>
2018-08-26 03:11:39 +0200

    find the middle point between bot and up tooth. preparing to applying pca

--- auto_init.py
+++ auto_init.py
@@ -33,6 +33,8 @@ def first_the_first_level_rect(crop_length, x_box_length, y_box_length):
     :param y_box_length:
     :return: the min block top left point index from the ORIGINAL image
     """
+    off_set_y = 200
+    off_set_x = 240
     rect_firt = []
     crop_length
     #x_box_start = 200
@@ -70,15 +72,16 @@ def first_the_first_level_rect(crop_length, x_box_length, y_box_length):
         #print mean
         #print()
         index_min_mean[picture] = np.where(mean == np.amin(mean, axis = (0,1)))
-        real_index_min_mean[picture] = index_min_mean[picture]*10
+        real_index_min_mean[picture][0] = index_min_mean[picture][0] + off_set_x
+        real_index_min_mean[picture][1] = index_min_mean[picture][1] + off_set_y
         #print index_min_mean
-        rect_tmp_for_show = img[int(index_min_mean[picture][1]) + 200: int((index_min_mean[picture][1]+200 + y_box_length)),
-                           int(index_min_mean[picture][0])+ 200:int((index_min_mean[picture][0]+200 + x_box_length))]
-        rect_firt.append((img[int(index_min_mean[picture][1]) + 200: int((index_min_mean[picture][1]+200 + y_box_length)),
-                           int(index_min_mean[picture][0])+ 200:int((index_min_mean[picture][0]+200 + x_box_length))]))
+        rect_tmp_for_show = img[int(real_index_min_mean[picture][1]): int(real_index_min_mean[picture][1] + y_box_length),
+                                int(real_index_min_mean[picture][0]):int((real_index_min_mean[picture][0] + x_box_length))]
+        rect_firt.append((img[int(real_index_min_mean[picture][1]): int(real_index_min_mean[picture][1] + y_box_length),
+                              int(real_index_min_mean[picture][0]):int((real_index_min_mean[picture][0] + x_box_length))]))
        # cv2.imshow('min_mean area', rect_tmp_for_show)
         #cv2.waitKey(500)
-        #cv2.imwrite('Data\Configure\init_guess_image-%d.tif' % picture, rect_tmp_for_show)
+        cv2.imwrite('Data\Configure\init_guess_image-%d.tif' % picture, rect_tmp_for_show)
     return rect_firt, real_index_min_mean
 
 def the_second_level_rect(rect,x_box_length, y_box_length, min_index_for_first_level):
@@ -104,22 +107,21 @@ def the_second_level_rect(rect,x_box_length, y_box_length, min_index_for_first_l
         # print mean
         # print()
         index_min_mean[picture] = np.where(mean == np.amin(mean, axis=(0, 1)))
-        real_index_min_mean[picture] = index_min_mean[picture]*10
-        rect_tmp_for_show = rect[picture][int(index_min_mean[picture][1]+off_set_y):int((index_min_mean[picture][1] + y_box_length+ off_set_y)),
-                                          int(index_min_mean[picture][0]+off_set_x):int((index_min_mean[picture][0]+ x_box_length)+off_set_x)]
-        rect_second.append((rect[0][int(index_min_mean[picture][1]+off_set_y): int((index_min_mean[picture][1] + y_box_length + off_set_y)),
-                                    int(index_min_mean[picture][0]+off_set_x): int((index_min_mean[picture][0] + x_box_length)+off_set_x)]))
-        cv2.imshow('min_mean area', rect_tmp_for_show)
-        cv2.waitKey(500)
+        real_index_min_mean[picture][0] = index_min_mean[picture][0] + off_set_x
+        real_index_min_mean[picture][1] = index_min_mean[picture][1] + off_set_y
+        rect_tmp_for_show = rect[picture][int(real_index_min_mean[picture][1]): int(real_index_min_mean[picture][1] + y_box_length),
+                                int(real_index_min_mean[picture][0]):int((real_index_min_mean[picture][0] + x_box_length))]
+        rect_second.append((rect[picture][int(real_index_min_mean[picture][1]): int(real_index_min_mean[picture][1] + y_box_length),
+                              int(real_index_min_mean[picture][0]):int((real_index_min_mean[picture][0] + x_box_length))]))
+       # cv2.imshow('min_mean area', rect_tmp_for_show)
+       # cv2.waitKey(500)
         cv2.imwrite('Data\Configure\init_guess_second_level-%d.tif' % picture, rect_tmp_for_show)
     return rect_second, real_index_min_mean
 
 def find_the_median_point_of_rect(rect):
-    print np.shape(rect)[1]
     median_point = np.zeros((np.shape(rect)[0], 2))
     for picture in range(np.shape(rect)[0]):
         median_point[picture] = [np.shape(rect)[1]/2, np.shape(rect)[2]/2]
-    print median_point
     return median_point
 
 
@@ -129,15 +131,36 @@ if __name__ =='__main__':
     #print min_index_for_first_level
     #print first_rect[0]
     second_rect, min_index_for_second_level = the_second_level_rect(first_rect, 440, 200, min_index_for_first_level)
+ #   print(np.shape(second_rect))
    #print min_index_for_second_level
     real_start_point = min_index_for_first_level + min_index_for_second_level
-    print(find_the_median_point_of_rect)
-    middel_point = [real_start_point[0] + find_the_median_point_of_rect(second_rect)[0],
-                    real_start_point[1] + find_the_median_point_of_rect(second_rect)[1]]
+    print np.shape(min_index_for_second_level)
+    print np.shape(min_index_for_first_level)
 
+   # print min_index_for_first_level[0, :]
+    #print min_index_for_second_level[0, :]
+    print np.shape(real_start_point) # since here it is right
+    #print real_start_point
+   # middel_point = [real_start_point[0] + find_the_median_point_of_rect(second_rect)[0], real_start_point[1] + find_the_median_point_of_rect(second_rect)[1]]
+    final_middle_point = np.zeros((14, 2))
+    for picture in range(14):
+        final_middle_point[picture] = [real_start_point[picture, 0] + 220, real_start_point[picture, 1] + 100]
+    for picture in range(14):
+        source = 'Data\Radiographs\%02d.tif' % (picture + 1)
+        raw_img = cv2.imread(source, 0)
+        cropped_img = crop(raw_img, 1000, 500, 1000, 1000)
+        img = median_filter(cropped_img)
+        img = bilateral_filter(img)
+        #print raw_img
+        img = top_hat_transform(img)
+        img = bottom_hat_transform(img)
+        img = sobel(img)
+        cv2.circle(img, (int(final_middle_point[picture, 0]), int(final_middle_point[picture, 1])), 10, (255, 0, 0), 1)
+        cv2.imshow('center circle', img)
+        cv2.waitKey(500)
+        cv2.imwrite('Data\Configure\init_guess_center_point-%d.tif' % picture, img)
 
-
-    print middel_point
+    #print final_middel_point
     #print(np.shape(first_rect))
 
 
